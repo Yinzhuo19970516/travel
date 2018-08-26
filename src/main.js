3a474a6992d9d210a69829ebe09a2d496ec99a2f
yinzhuo <2572703497@qq.com>
2018-08-26 23:06:19 +0800

    ”去哪儿“

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//解决浏览器兼容问题
+import 'styles/reset.css'
+// 1像素边框既然决方案
+import 'styles/border.css'
+//解决300ms 点击延迟
+import fastClick from 'fastclick'
+import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import store from './store'
+
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
+
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
